a4eff37eecabb82e1e7a51e8df987736ce5b7f2d
Trieu Thanh <tvthanh.cod@gmail.com>
2024-07-03 05:15:50 +0700

    feat(route): route product material

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRouter = require("./auth.route");
+const productMaterialRouter = require("./product-material.route");
 
 router.use("/auth", authRouter);
+router.use("/product-material", productMaterialRouter);
 
 module.exports = router;
---
+++ src/routes/product-material.route.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const {
+  addMaterial,
+  updateMaterial,
+  removeMaterial,
+} = require("../controllers/product-material.controller");
+const { authorize, isAmin } = require("../middleware/auth");
+
+const productMaterialRouter = express.Router();
+
+productMaterialRouter.get("/", async (req, res) => {
+  try {
+    const data = await db.Material.findAll();
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 1,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+productMaterialRouter.post("/add", authorize, isAmin, async (req, res) => {
+  const { materialName } = req.body;
+
+  try {
+    const newMaterial = await addMaterial(materialName);
+    res.status(201).json({
+      id: newMaterial.id,
+      message: "Created New Product Material",
+    });
+  } catch (error) {
+    res.status(503).json({
+      error: 1,
+      message: "Service Unavailable",
+    });
+  }
+});
+productMaterialRouter.put("/update", authorize, isAmin, async (req, res) => {
+  const { id, materialName } = req.body;
+  try {
+    await updateMaterial(id, materialName);
+    res.status(204).json({
+      message: "Updated Product Material",
+    });
+  } catch (error) {
+    res.status(503).json({
+      error: 1,
+      message: "Service Unavailable",
+    });
+  }
+});
+productMaterialRouter.delete("/", authorize, isAmin, async (req, res) => {
+  const { id } = req.body;
+  try {
+    await removeMaterial(id);
+    res.status(204).json({
+      message: "Removed Product Material",
+    });
+  } catch (error) {
+    res.status(503).json({
+      error: 1,
+      message: "Service Unavailable",
+    });
+  }
+});
+
+module.exports = productMaterialRouter;
